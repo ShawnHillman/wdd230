4b31a82c59f9fd1921e049ca2ece945ac251c10b
ShawnHillman <schmea32@gmail.com>
2022-02-22 11:39:35 -0700

    .

--- chamber4/js/script.js
+++ chamber4/js/script.js
@@ -1,24 +1,18 @@
-// const today = new Date();
+// Get Dates
 
-// const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-// const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const displayDate = document.querySelector("#header-date");
+const displayMod = document.querySelector('#lastmod');
 
-// const currentDay = 
+const today = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
 
+displayDate.innerHTML = `${fulldate}`;
+displayMod.innerHTML = document.lastModified;
 
-// const displayDate = document.querySelector("#header-date");
-// const displayMod = document.querySelector('#lastmod');
-
-// const today = new Date();
-// const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
-
-// displayDate.innerHTML = `${fulldate}`;
-// displayMod.innerHTML = document.lastModified;
-
-// if (today.getDay !== 1 && today.getDay !== 2) {
-//     document.getElementById('m-banners').style.display = "none";
-//     document.getElementsByTagName('main').style.gridTemplateRows = "0px 300px 300px 300px";
-// }
+if (today.getDay !== 1 && today.getDay !== 2) {
+    document.getElementById('m-banners').style.display = "none";
+    document.getElementsByTagName('main').style.gridTemplateRows = "0px 300px 300px 300px";
+}
 
 // // Windchill Calculation
 
