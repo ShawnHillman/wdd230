f777df9ee197c2125dffe68db96cdf96d9069f6b
Shawn Hillman <schmea32@gmail.com>
2022-10-14 15:56:50 -0600

    .

--- bom/js/bom.js
+++
@@ -1,9 +0,0 @@
-function addChapter() {
-
-    let chapter = document.getElementById("addNew");
-    document.getElementById("scripture-list").innerHTML += 
-    "<li><p></li>"
-
-    console.log(chapters);
-
-}
