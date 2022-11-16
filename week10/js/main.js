852ca02c5bbf377402dfdbaf3d8df6a6288e54f5
Shawn Hillman <schmea32@gmail.com>
2022-11-16 11:00:39 -0700

    weather api finish

--- week10/js/main.js
+++ week10/js/main.js
@@ -4,5 +4,16 @@ const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
     console.log(jsObject);
-  };
+    let kelvin = jsObject.main.temp;
+    let farenheit = (kelvin -273.15)* 1.8 + 32;
+    document.querySelector("#current-temp").textContent = farenheit.toFixed(2);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+};
+
 getWeather();
