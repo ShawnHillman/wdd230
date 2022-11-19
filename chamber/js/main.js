616bb3e6843b07c0dc373774000705ba1986b897
shawnhillman <schmea32@gmail.com>
2022-11-18 23:13:17 -0700

    weather api

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,30 +1,43 @@
 function viewNav() {
     let nav = document.querySelector("#navigation");
-    nav.classList.toggle("up");
     let content = document.querySelector("main");
-    content.classList.toggle("up");
     let footer = document.querySelector("footer");
+
+    nav.classList.toggle("up");
+    content.classList.toggle("up");
     footer.classList.toggle("up");
 }
 
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=b650d442616f7081512ff0ae0fb0b7ac";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5596475&appid=b650d442616f7081512ff0ae0fb0b7ac";
+
+function windChill(kelvin) {
+    return Math.round(1.8 * (kelvin - 273.15) + 32);
+}
 
 const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
     console.log(jsObject);
+
     let kelvin = jsObject.main.temp;
-    let farenheit = (kelvin -273.15)* 1.8 + 32;
-    farenheit = Math.round(farenheit);
-    console.log(farenheit);
+    // console.log(jsObject.main.temp);
+    let farenheit = windChill(kelvin);
+    // console.log(farenheit);
     document.querySelector("#current-temp").textContent = farenheit;
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
-    document.querySelector('#icon-src').textContent = iconsrc;
+
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
     document.querySelector('#weathericon').setAttribute('alt', desc);
-    document.querySelector('figcaption').textContent = desc;
+    document.querySelector('#weather-type').textContent = desc;
+    document.querySelector('#wind-speed').textContent = jsObject.wind.speed;
+    let windchill = document.querySelector('#wind-chill');
+    if(jsObject.wind.speed === 0) {
+        windchill.textContent = "N/A";
+    } else {
+        windchill.textContent = windChill(jsObject.main.feels_like) + "°";
+    }
 };
 
 getWeather();
