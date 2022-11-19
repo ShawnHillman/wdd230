6cd16d681618a53ebd4856b148f1468851bb7ceb
shawnhillman <schmea32@gmail.com>
2022-11-18 17:21:20 -0700

    submission

--- week05/js/main.js
+++ week05/js/main.js
@@ -6,3 +6,25 @@ function viewNav() {
     let footer = document.querySelector("footer");
     footer.classList.toggle("up");
 }
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=b650d442616f7081512ff0ae0fb0b7ac";
+
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const jsObject = await response.json();
+    console.log(jsObject);
+    let kelvin = jsObject.main.temp;
+    let farenheit = (kelvin -273.15)* 1.8 + 32;
+    farenheit = Math.round(farenheit);
+    console.log(farenheit);
+    document.querySelector("#current-temp").textContent = farenheit;
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+};
+
+getWeather();
